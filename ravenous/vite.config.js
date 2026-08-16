c43679a2fdc283553b22c7ebc282c2a3164b4e51
serenity93 <jessica.aimee.smith@gmail.com>
2026-08-16 22:41:19 +0930

    Additional configuration for readiness of Yelp API.

--- ravenous/vite.config.js
+++ ravenous/vite.config.js
@@ -1,8 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv} from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+export default defineConfig(({ mode }) => {
+  // Load env file based on the current mode (development, production, etc.)
+  // The third argument '' ensures all variables are loaded, not just those prefixed with VITE_
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: `${env.VITE_YELP_API_URL}`,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          configure: (proxy) => {
+            proxy.on('proxyReq', (proxyReq) => {
+              // Inject the key securely from your .env file
+              if (env.YELP_API_KEY) {
+                proxyReq.setHeader('Authorization', `Bearer ${env.VITE_YELP_API_KEY}`);
+              }
+            });
+          },
+        },
+      },
+    },
+  };
+});
